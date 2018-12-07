ca3b7494807a01f0dcebe269b4728f5457b52c02
Kailin Huang <kailin-huang@outlook.com>
2018-12-07 16:06:32 +0300

    day 5 task 3

--- day5/advent5.cpp
+++ day5/advent5.cpp
@@ -10,22 +10,26 @@ bool react(char a, char b){
     return (a-b==32 || b-a==32);
 }
 
-int main(){
+void readIn(ifstream& in, char& var, int letter){
+    do{
+        in >> var;
+    }while(var==letter || var == letter+32);
+}
 
-ifstream in;
-
-
-in.open("input.txt");
-char curr;
-char next;
-
+int checkEndLength(string fileName, int letter){
+    cout<<(char)letter<<endl;
+    int res = 0;
+    ifstream in;
+    in.open(fileName);
+    char curr;
+    char next;
     stack<char> output;
-
-    in >> curr;
+    
+    readIn(in, curr, letter);
 
     while(!in.eof()){
-        in >> next;
-        // cout<<curr<<next<<endl;
+        readIn(in, next, letter);
+
         if(!react(curr, next)){
             if(!output.empty()&&react(curr, output.top())){
                 output.pop();
@@ -37,15 +41,35 @@ char next;
                 curr=next;  
             }     
         }else if(react(curr,next)){
-            in>>curr;
+            readIn(in, curr, letter);
         }
     }
 
-    std::cout<<output.size();
-    while(!output.empty()){
-        std::cout<<output.top();
-        output.pop();
-    }
+    res = output.size();
+    // std::cout<<output.size();
+    // while(!output.empty()){
+    //      std::cout<<output.top();
+    //     output.pop();
+    // }
+
+
+    return res;
+}
+
+int main(){
+
+
+int maxLength = 2147483647;
+
+for(int i = 65; i <= 90; i++){
+    int tmp = checkEndLength("input.txt", i);
+    cout<<tmp<<endl;
+    if(maxLength>tmp) maxLength = tmp;
+}
+
+
+cout<< endl<<"best = " <<maxLength<<endl;
+   
 
 return 0;
 }
